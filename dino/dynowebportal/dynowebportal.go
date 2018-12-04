2bf02c4f6073a360227e5471f7d4b721a051ed1b
Geunyoung Kim <kgysmart@gmail.com>
2018-12-03 23:02:57 -0800

    Add dynowebportal

---
+++ dino/dynowebportal/dynowebportal.go
@@ -0,0 +1,15 @@
+package dynowebportal
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func RunWebPortal(addr string) error {
+	http.HandleFunc("/", roothandler)
+	return http.ListenAndServe(addr, nil)
+}
+
+func roothandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the Dino web portal %s", r.RemoteAddr)
+}
