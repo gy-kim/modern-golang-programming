46737f9db04c32f1ad0f220b2b64f347ea3bff55
Geunyoung Kim <kgysmart@gmail.com>
2018-12-05 17:41:09 -0800

    Implements mongodbhandler

--- dino/databaselayer/mongodbhandler.go
+++ dino/databaselayer/mongodbhandler.go
@@ -1,24 +1,57 @@
 package databaselayer
 
-type MongodbHandler struct{}
+import (
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
 
-func NewMongodbHandler() *MongodbHandler {
-	return nil
+type MongodbHandler struct {
+	*mgo.Session
+}
+
+func NewMongodbHandler(connection string) (*MongodbHandler, error) {
+	s, err := mgo.Dial(connection)
+	return &MongodbHandler{
+		Session: s,
+	}, err
 }
 
 func (handler *MongodbHandler) GetAvailableDynos() ([]Animal, error) {
-	return nil, nil
+	s := handler.getFreshSession()
+	defer s.Close()
+	animals := []Animal{}
+	err := s.DB("Dino").C("animals").Find(nil).All(&animals)
+	return animals, err
 }
 
-func (handler *MongodbHandler) GetDynoByNickname(string) (Animal, error) {
-	return Animal{}, nil
+func (handler *MongodbHandler) GetDynoByNickname(nickname string) (Animal, error) {
+	s := handler.getFreshSession()
+	defer s.Close()
+	a := Animal{}
+	err := s.DB("Dino").C("animals").Find(bson.M{"nicknake": nickname}).One(&a)
+	return a, err
 }
-func (handler *MongodbHandler) GetDynosByType(string) (Animal, error) {
-	return Animal{}, nil
+
+func (handler *MongodbHandler) GetDynosByType(dinoType string) ([]Animal, error) {
+	s := handler.getFreshSession()
+	defer s.Close()
+	animals := []Animal{}
+	err := s.DB("Dino").C("animals").Find(bson.M{"animal_type": dinoType}).All(&animals)
+	return animals, err
 }
-func (handler *MongodbHandler) AddAnimal(Animal) error {
-	return nil
+
+func (handler *MongodbHandler) AddAnimal(a Animal) error {
+	s := handler.getFreshSession()
+	defer s.Close()
+	return s.DB("Dino").C("animals").Insert(a)
 }
-func (handler *MongodbHandler) UpdateAnumal(Animal, string) error {
-	return nil
+
+func (handler *MongodbHandler) UpdateAnimal(a Animal, nname string) error {
+	s := handler.getFreshSession()
+	defer s.Close()
+	return s.DB("Dino").C("animals").Update(bson.M{"nickname": nname}, a)
+}
+
+func (handler *MongodbHandler) getFreshSession() *mgo.Session {
+	return handler.Session.Copy()
 }
