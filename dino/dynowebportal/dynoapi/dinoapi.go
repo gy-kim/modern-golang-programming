81dbf36a7fc0167c5b921ded8ee1ce8d25cf9cc1
Geunyoung Kim <kgysmart@gmail.com>
2018-12-06 22:26:01 -0800

    Implement api

--- dino/dynowebportal/dynoapi/dinoapi.go
+++ dino/dynowebportal/dynoapi/dinoapi.go
@@ -1,1 +1,23 @@
 package dynoapi
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/gy-kim/modern-golang-programming/dino/databaselayer"
+)
+
+func RunApi(endpoint string, db databaselayer.DinoDBHandler) error {
+	r := mux.NewRouter()
+	RunAPIOnRouter(r, db)
+	return http.ListenAndServe(endpoint, r)
+}
+
+func RunAPIOnRouter(r *mux.Router, db databaselayer.DinoDBHandler) {
+	handler := newDinoRESTAPIHandler(db)
+
+	apirouter := r.PathPrefix("/api/dinos").Subrouter()
+
+	apirouter.Methods("GET").Path("/{SearchCriterial}/{search}").HandlerFunc(handler.searchHandler)
+	apirouter.Methods("POST").PathPrefix("/{Operation}").HandlerFunc(handler.editsHandler)
+}
